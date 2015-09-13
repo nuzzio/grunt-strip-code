2a699b4e553b2fe289ae33004a24b1e0eb0d4ca4
Rene Cabral <rene@oki.io>
2015-09-13 13:48:22 -0400

    refactored main test

--- test/strip_code_test.js
+++ test/strip_code_test.js
@@ -1,81 +1,90 @@
+/**
+ * grunt-strip-code
+ * https://github.com/nuzzio/grunt-strip-code
+ *
+ * Copyright (c) 2015 Rene Cabral
+ * Licensed under the MIT license.
+ * Date: 9/13/15
+ */
+
 'use strict';
 
-var grunt = require('grunt');
+var helper = require('./helper.js').helper;
 
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+ ======== A Handy Little Nodeunit Reference ========
+ https://github.com/caolan/nodeunit
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+ Test methods:
+ test.expect(numAssertions)
+ test.done()
+ Test assertions:
+ test.ok(value, [message])
+ test.equal(actual, expected, [message])
+ test.notEqual(actual, expected, [message])
+ test.deepEqual(actual, expected, [message])
+ test.notDeepEqual(actual, expected, [message])
+ test.strictEqual(actual, expected, [message])
+ test.notStrictEqual(actual, expected, [message])
+ test.throws(block, [error], [message])
+ test.doesNotThrow(block, [error], [message])
+ test.ifError(value)
+ */
 
 exports.strip_code = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
+    setUp: function (done) {
+        // setup here if necessary
+        done();
+    },
+    //default_options: function (test) {
+    //    test.expect(1);
+    //
+    //    var actual = helper.grunt.file.read('default_options.js');
+    //    var expected = helper.grunt.file.read('default_options.js');
+    //    test.equal(actual, expected, 'should default to using "test-code" and "end-test-code" as the identifiers.');
+    //
+    //    test.done();
+    //},
+    start_end_options: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options.js');
-    var expected = grunt.file.read('test/expected/default_options.js');
-    test.equal(actual, expected, 'should default to using "test-code" and "end-test-code" as the identifiers.');
+        var actual = helper.tmpReader('start_end_options.js');
+        var expected = helper.expectedReader('start_end_options.js');
+        test.equal(actual, expected, 'should use the specified start and end comment identifiers.');
 
-    test.done();
-  },
-  start_end_options: function(test) {
-    test.expect(1);
+        test.done();
+    },
+    pattern_options: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/start_end_options.js');
-    var expected = grunt.file.read('test/expected/start_end_options.js');
-    test.equal(actual, expected, 'should use the specified start and end comment identifiers.');
+        var actual = helper.tmpReader('pattern_options.js');
+        var expected = helper.expectedReader('pattern_options.js');
+        test.equal(actual, expected, 'should ignore start and end identifiers and use the pattern option if present.');
 
-    test.done();
-  },
-  pattern_options: function(test) {
-    test.expect(1);
+        test.done();
+    },
+    dest_specified: function (test) {
+        test.expect(1);
 
-    var actual = grunt.file.read('tmp/pattern_options.js');
-    var expected = grunt.file.read('test/expected/pattern_options.js');
-    test.equal(actual, expected, 'should ignore start and end identifiers and use the pattern option if present.');
+        var actual = helper.tmpReader('dest_specified2.js');
+        var expected = helper.expectedReader('dest_specified.js');
+        test.equal(actual, expected, 'should save to the dest option if specified.');
 
-    test.done();
-  },
-  dest_specified: function(test) {
-    test.expect(1);
+        test.done();
+    },
+    multiple_files: function (test) {
+        test.expect(3);
 
-    var actual = grunt.file.read('tmp/dest_specified2.js');
-    var expected = grunt.file.read('test/expected/dest_specified.js');
-    test.equal(actual, expected, 'should save to the dest option if specified.');
+        var actual1 = helper.tmpReader('multiple_files1.js');
+        var expected1 = helper.expectedReader('multiple_files1.js');
+        var actual2 = helper.tmpReader('multiple_files2.js');
+        var expected2 = helper.expectedReader('multiple_files2.js');
+        var actual3 = helper.tmpReader('another_multiple_file.js');
+        var expected3 = helper.expectedReader('another_multiple_file.js');
+        test.equal(actual1, expected1, 'should handle multiple files.');
+        test.equal(actual2, expected2, 'should handle multiple files.');
+        test.equal(actual3, expected3, 'should handle multiple files.');
 
-    test.done();
-  },
-  multiple_files: function(test) {
-    test.expect(3);
-
-    var actual1 = grunt.file.read('tmp/multiple_files1.js');
-    var expected1 = grunt.file.read('test/expected/multiple_files1.js');
-    var actual2 = grunt.file.read('tmp/multiple_files2.js');
-    var expected2 = grunt.file.read('test/expected/multiple_files2.js');
-    var actual3 = grunt.file.read('tmp/another_multiple_file.js');
-    var expected3 = grunt.file.read('test/expected/another_multiple_file.js');
-    test.equal(actual1, expected1, 'should handle multiple files.');
-    test.equal(actual2, expected2, 'should handle multiple files.');
-    test.equal(actual3, expected3, 'should handle multiple files.');
-
-    test.done();
-  },
+        test.done();
+    }
 };
